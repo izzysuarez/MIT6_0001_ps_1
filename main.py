544593d33e9347ea47c47349944d979984f5875e
Isaac Suarez <>
2022-12-27 01:40:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,60 @@
+# import statments:
+import math
+
+# declare variables and initialize
+
+down_payment_perc = 0.25
+current_savings = 0.0
+months_saving = 0
+apr = 0.04
+
+
+# define a function that takes in the current savings and adds the apr/12 to the current amount
+def monthly_return(current_savings):
+  current_savings = current_savings + ((current_savings * apr) / 12)
+  return current_savings
+
+
+# get input from the user to define the annual salary and monthly savings.
+annual_salary = float(input("What is your annual salary?: "))
+perc_saved = float(
+  input("Enter the percent of your salary to save, as a decimal: "))
+
+monthly_salary = annual_salary / 12
+monthly_savings = monthly_salary * perc_saved
+
+# define the cost of the dream home
+total_cost = float(input("Enter the cost of our dream home: "))
+portion_down_payment = total_cost * down_payment_perc
+# print()
+# print("Down Payment Due: ")
+# print(portion_down_payment)
+
+# define the semi-annual raise amount as a percentage
+semi_annual_raise = float(input("Enter the semi-annual raise, as a decimal: "))
+
+while (current_savings <= portion_down_payment):
+  # add monthly savings to current savings
+  current_savings += monthly_savings
+
+  # add monthly return to current savings
+  current_savings = monthly_return(current_savings)
+
+  # increment month counter by one
+  months_saving += 1
+
+  # add if stament that redefines the annual salary every 6 months.
+  if ((months_saving % 6) == 0):
+    # print(months_saving)
+    annual_salary = annual_salary * (1 + semi_annual_raise)
+    monthly_salary = annual_salary / 12
+    monthly_savings = monthly_salary * perc_saved
+    
+
+# print final savings amount and months saved
+print()
+# print("Final Savings Amt: ")
+# print(round(current_savings, 2))
+# print()
+# print("Total Months Saved: ")
+print("Number of months:", months_saving)
