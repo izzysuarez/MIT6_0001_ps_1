045bf5afd5b8b060f1dbb3194c7e7a82f02c8d73
Isaac Suarez <suareisa000@gmail.com>
2023-01-05 22:47:57 +0000

    A brute force method to find the first percentage that provides the saving amount in 36 months

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 # import statments:
 import math
+import numpy as np
 
-# declare variables and initialize
+# **************************
+#  ----- GLOBAL VARIABLES
+# **************************
 
-down_payment_perc = 0.25
-current_savings = 0.0
-months_saving = 0
+# Global variables for the annual salary and portion to save in decimal. BASED ON USER INPUT..
+annual_salary = float(input("What is your annual salary?: "))
 apr = 0.04
-
+semi_annual_raise_rate = 0.07
 
 # define a function that takes in the current savings and adds the apr/12 to the current amount
 def monthly_return(current_savings):
@@ -15,44 +17,58 @@ def monthly_return(current_savings):
   return current_savings
 
 
-# get input from the user to define the annual salary and monthly savings.
-annual_salary = float(input("What is your annual salary?: "))
-portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
-
-monthly_salary = annual_salary / 12
-monthly_savings = monthly_salary * portion_saved
-
-# define the cost of the dream home
-total_cost = float(input("Enter the cost of our dream home: "))
-portion_down_payment = total_cost * down_payment_perc
-# print()
-# print("Down Payment Due: ")
-# print(portion_down_payment)
-
-# define the semi-annual raise amount as a percentage
-semi_annual_raise = float(input("Enter the semi-annual raise, as a decimal: "))
-
-while (current_savings <= portion_down_payment):
+def number_of_months(perc_rate, ann_Sal, apr_rate, sa_rr):
+  # declare variables and initialize
+  down_payment_perc = 0.25
+  current_savings = 0.0
+  months_saving = 0
+  apr = apr_rate
   
-  # add interest on current savings to the current savings variable
-  current_savings = monthly_return(current_savings)
+  # # define the cost of the dream home
+  total_cost = float(1000000)
+  portion_down_payment = total_cost * down_payment_perc
+  
+  # define the semi-annual raise amount as a percentage
+  semi_annual_raise = float(sa_rr)
+  
+  
+  monthly_salary = ann_Sal / 12
+  portion_saved = float(perc_rate)
+  monthly_savings = monthly_salary * portion_saved
 
-  # add a portion of the monthy salary to the current savings
-  current_savings += monthly_savings
+  if(monthly_salary > (portion_down_payment/36)):
+    while (current_savings <= portion_down_payment):
+      
+      # add interest on current savings to the current savings variable
+      current_savings = monthly_return(current_savings)
+    
+      # add a portion of the monthy salary to the current savings
+      current_savings += monthly_savings
+    
+      # increment month counter by one
+      months_saving += 1
+    
+      # add if stament that redefines the annual salary every 6 months.
+      if ((months_saving % 6) == 0):
+        # print(months_saving)
+        ann_Sal = ann_Sal * (1 + semi_annual_raise)
+        monthly_salary = ann_Sal / 12
+        monthly_savings = monthly_salary * portion_saved
+    
+    # print("Rate:", months_saving)
+    return months_saving
+  else:
+    print("It is not possible to pay the down payment in three years.")
+    return None
 
-  # increment month counter by one
-  months_saving += 1
+for x in np.arange(0,1,0.0001):
+  ms = number_of_months(float(x), annual_salary, apr, semi_annual_raise_rate)
+  if(ms is None):
+    break
+  elif (ms <= 36):
+    print("Rate:",round(x, 4))
+    print("Number of months: __",ms)
+    break
 
-  # add if stament that redefines the annual salary every 6 months.
-  if ((months_saving % 6) == 0):
-    # print(months_saving)
-    annual_salary = annual_salary * (1 + semi_annual_raise)
-    monthly_salary = annual_salary / 12
-    monthly_savings = monthly_salary * portion_saved
 
-# print final savings amount and months saved
-# print("Final Savings Amt: ")
-# print(round(current_savings, 2))
-# print()
-# print("Total Months Saved: ")
-print("Number of months:", months_saving)
+    
