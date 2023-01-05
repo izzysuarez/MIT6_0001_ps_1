284b49a389814b5e9bf317ee72b2680423895b27
Isaac Suarez <suareisa000@gmail.com>
2023-01-05 18:35:00 +0000

    Initial commit

--- main.py
+++ main.py
@@ -17,11 +17,10 @@ def monthly_return(current_savings):
 
 # get input from the user to define the annual salary and monthly savings.
 annual_salary = float(input("What is your annual salary?: "))
-perc_saved = float(
-  input("Enter the percent of your salary to save, as a decimal: "))
+portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
 
 monthly_salary = annual_salary / 12
-monthly_savings = monthly_salary * perc_saved
+monthly_savings = monthly_salary * portion_saved
 
 # define the cost of the dream home
 total_cost = float(input("Enter the cost of our dream home: "))
@@ -34,11 +33,12 @@ portion_down_payment = total_cost * down_payment_perc
 semi_annual_raise = float(input("Enter the semi-annual raise, as a decimal: "))
 
 while (current_savings <= portion_down_payment):
-  # add monthly savings to current savings
-  current_savings += monthly_savings
-
-  # add monthly return to current savings
+  
+  # add interest on current savings to the current savings variable
   current_savings = monthly_return(current_savings)
+
+  # add a portion of the monthy salary to the current savings
+  current_savings += monthly_savings
 
   # increment month counter by one
   months_saving += 1
@@ -48,11 +48,9 @@ while (current_savings <= portion_down_payment):
     # print(months_saving)
     annual_salary = annual_salary * (1 + semi_annual_raise)
     monthly_salary = annual_salary / 12
-    monthly_savings = monthly_salary * perc_saved
-    
+    monthly_savings = monthly_salary * portion_saved
 
 # print final savings amount and months saved
-print()
 # print("Final Savings Amt: ")
 # print(round(current_savings, 2))
 # print()
